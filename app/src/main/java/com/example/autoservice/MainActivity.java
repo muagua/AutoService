944fe0c72001d98567a8ba57aceaf398b628e87c
muagua <1483708664@qq.com>
2016-12-13 17:29:13 +0800

    优化

--- app/src/main/java/com/example/autoservice/MainActivity.java
+++ app/src/main/java/com/example/autoservice/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.autoservice;
 
 import android.app.Activity;
-import android.app.NotificationManager;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
